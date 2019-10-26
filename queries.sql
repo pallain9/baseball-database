1f96f22a4758826f5fe801b1ecda526fecfc3e78
Peter allain <pballain910@gmail.com>
2019-10-26 19:50:06 -0400

    queries complete

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+USE baseball;
+
 SELECT * FROM teams;
 
 UPDATE teams 
@@ -12,3 +14,47 @@ SELECT startTime, homeScore, awayScore
 Select id, league, abbreviation, mascot
 FROM teams
 WHERE league = "NL";
+
+SELECT CONCAT (firstName, ' ', lastName) AS FUllName, position, hits, throws, t.abbreviation
+   FROM players p
+   JOIN teams t
+   ON p.currentTeamId = t.id WHERE abbreviation ='BOS';
+   
+SELECT CONCAT (firstName, ' ', lastName) AS FullName, position, t.abbreviation, homeRuns
+ FROM hitterStats h
+ JOIN players p
+ ON p.id = h.playerId
+ JOIN teams t
+ ON p.currentTeamId =t.id
+ JOIN games g
+ ON h.gameId = g.id WHERE startTime >= "2019-06-06 00:00:00" AND startTime <= "2019-06-06 17:00:00"
+ ORDER BY h.homeRuns DESC LIMIT 5;
+ 
+SELECT CONCAT (firstName, ' ', lastName) AS FullName, position, t.abbreviation, AVG (earnedRuns*9/inningsPitched) AS ERA
+ FROM pitcherStats s 
+ JOIN players p 
+ ON p.id = s.playerId 
+ JOIN teams t
+ ON p.currentTeamId =t.id 
+ JOIN games g
+ ON s.gameId = g.id WHERE startTime >= "2019-06-06 00:00:00" AND startTime <= "2019-06-06 23:59:59" AND inningsPitched >= 4
+ Group BY p.id ORDER BY ERA ASC LIMIT 5;   
+ 
+ SELECT CONCAT (firstName, ' ', lastName) AS FullName, position, t.abbreviation, doubles, g.startTime, (SELECT abbreviation FROM teams WHERE homeTeamId = teams.id) AS HomeTeam, (SELECT abbreviation FROM teams WHERE awayTeamId = teams.id) AS AwayTeam
+ FROM hitterStats h
+ JOIN players p
+ ON p.id = h.playerId
+ JOIN teams t
+ ON p.currentTeamId =t.id
+ JOIN  games g
+ ON h.gameId = g.id WHERE startTime >= "2019-06-06 00:00:00" AND startTime <= "2019-06-06 23:59:59"
+ ORDER BY h.doubles DESC LIMIT 1;
+ 
+ 
+ select lastName, position, inningsPitched, earnedRuns
+ from pitcherStats s
+ join players p
+ on p.id = s.playerId;
+ 
+ select *
+ from games;
